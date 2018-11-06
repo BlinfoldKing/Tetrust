802bcf27bff1c2b8c97e3512f8941967cc2e15de
Ganesha Danu <ganeshadanu1@gmail.com>
2018-11-07 00:25:56 +0700

    add event cycle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+extern crate rand;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new(
+        "Tetrust",
+        [800, 600]
+    )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut events = Events::new(EventSettings::new()).ups(5);
+    while let Some(e) = events.next(&mut window) {
+    }
 }
+
